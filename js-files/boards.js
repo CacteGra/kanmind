e35847a39c9131a7d8ac2fbbf32a5ba240000ec9
cactegra <cactegra@protonmail.com>
2025-11-10 17:12:35 +0100

    working ul buttons show hide with overlay

--- js-files/boards.js
+++ js-files/boards.js
@@ -7,6 +7,19 @@ boards = {
         }
     },
 
+
+    closeUlModal: function () {
+        const modal = document.getElementById('modalOverlay');
+        modal.classList.add('hide');
+        allUl = document.getElementsByTagName("ul");
+
+        var checkbox = document.getElementById("check01");
+        checkbox.checked = false;
+
+        var checkbox = document.getElementById("check02");
+            checkbox.checked = false;
+    },
+
     addOverlay: function() {
         document.getElementById("modalOverlay").classList.remove("hide");
     },
@@ -46,25 +59,27 @@ boards = {
     },
 
     listAllBoards: function() {
-	var listHtml = "";
-    var board;
-	keys = boards.getBoards;
-	console.log(keys);
-	keys = keys.filter(e => e !== 'lastBoard');
-    i = keys.length;
-    console.log(keys);
-    boardName = document.getElementById("boardName");
-	while ( i-- ) {
-        if (boardName.innerText !== keys[i]) {
-            listHtml += `<li onclick="boards.changeTaskBoard(keys[i])">${keys[i]}</li>`;
+        boards.addOverlay();
+        var listHtml = "";
+        keys = boards.getBoards();
+        console.log(keys);
+        if (keys) {
+            keys = keys.filter(e => e !== 'lastBoard');
+            i = keys.length;
+            console.log(keys);
+            boardName = document.getElementById("boardName");
+            while ( i-- ) {
+                if (boardName.innerText !== keys[i]) {
+                    listHtml += `<li onclick="boards.changeTaskBoard(keys[i])">${keys[i]}</li>`;
+                }
+            };
         }
-	};
-    if (listHtml === "") {
-        listHtml = `<li style="color: #acacac;">None.</li>`;
-    };
-    console.log(listHtml);
-	boardsList = document.getElementById("boardsListing");
-    boardsList.innerHTML = listHtml;
+        if (listHtml === "") {
+            listHtml = `<li style="color: #acacac;">None.</li>`;
+        };
+        console.log(listHtml);
+        boardsList = document.getElementById("boardsListing");
+        boardsList.innerHTML = listHtml;
     },
 
     changeTaskBoard: function(e) {
